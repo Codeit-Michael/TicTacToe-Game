683a144ea66a04a0181df54137b594a22fcef956
Michael Maranan <michaelmaranan030@gmail.com>
2023-03-19 00:41:57 +0800

    add game details

--- tictactoe.py
+++ tictactoe.py
@@ -3,16 +3,15 @@ import os
 import pygame
 from pygame.locals import *
 
-
 pygame.init()
 pygame.font.init()
 
 window_size = (450, 500)
-
 cell_size = 150
 
 screen = pygame.display.set_mode(window_size)
 pygame.display.set_caption("Tic Tac Toe")
+
 
 class TicTacToe():
 
@@ -20,14 +19,13 @@ class TicTacToe():
 		self.cell_size = cell_size
 		self.player = "X"
 		self.winner = None
+		self.taking_move = True
 		self.running = True
 		self.table = []
 		for col in range(3):
 			self.table.append([])
 			for row in range(3):
 				self.table[col].append("-")
-
-		self.draw_playah = None
 
 		self.background_color = (200, 200, 230)
 		self.table_color = (10, 20, 7)
@@ -38,31 +36,38 @@ class TicTacToe():
 
 	def draw_table(self):
 		# draw this part w/ math
-		r1 = pygame.draw.line(screen, self.table_color, [25, 153], [425, 153], 10)
-		r2 = pygame.draw.line(screen, self.table_color, [25, 287], [425, 287], 10)
-		c1 = pygame.draw.line(screen, self.table_color, [153, 25], [153, 425], 10)
-		c2 = pygame.draw.line(screen, self.table_color, [287, 25], [287, 425], 10)
+		r1 = pygame.draw.line(screen, self.table_color, [20, 150], [430, 150], 8)
+		r2 = pygame.draw.line(screen, self.table_color, [20, 300], [430, 300], 8)
+		c1 = pygame.draw.line(screen, self.table_color, [150, 20], [150, 430], 8)
+		c2 = pygame.draw.line(screen, self.table_color, [300, 20], [300, 430], 8)
 		instructions = self.font.render('Instructions here', True, (10,0,10))
 		screen.blit(instructions,(125,450))
 
 
 	def _change_player(self):
-		if self.player == "X":
-			self.player = "O"
-		else:
-			"X"
+		self.player = "O" if self.player == "X" else "X"
+
+
+	def draw_char(self, x, y, player):
+		if self.player == "O":
+			img = pygame.image.load("images/Tc-O.png")
+		elif self.player == "X":
+			img = pygame.image.load("images/Tc-X.png")
+		img = pygame.transform.scale(img, (self.cell_size, self.cell_size))
+		screen.blit(img, (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size))
 
 
 	def move(self, pos):
 		x, y = pos[0] // self.cell_size, pos[1] // self.cell_size
 		if self.table[x][y] == "-":
 			self.table[x][y] = self.player
+			self.draw_char(x,y,self.player)
 
 
 	def _message(self):
 		if self.winner is not None:
 			print(f"{self.winner} wins!!")
-		elif not self.running:
+		elif not self.taking_move:
 			print("Draw!!")
 
 
@@ -118,23 +123,24 @@ class TicTacToe():
 				if cell == "-":
 					blank_cells += 1
 		if blank_cells == 0:
-			self.running = False
+			self.taking_move = False
 			self._message()
 
 
 	def main(self):
 		screen.fill(self.background_color)
-		self.draw_table()
-		while self.running and self.winner == None:
+		while self.running:
+			self.draw_table()
 			mx, my = pygame.mouse.get_pos()
 			for self.event in pygame.event.get():
 				if self.event.type == pygame.QUIT:
 					self.running = False
 
 				if self.event.type == pygame.MOUSEBUTTONDOWN:
-					self.move(self.event.pos)
-					self._game_check()
-					self._change_player()
+					if self.taking_move:
+						self.move(self.event.pos)
+						self._game_check()
+						self._change_player()
 
 			pygame.display.flip()
 			self.FPS.tick(60)
@@ -142,31 +148,3 @@ class TicTacToe():
 
 f = TicTacToe(cell_size)
 f.main()
-
-
-
-
-"""
-yellow bg, orange sb, light blue instructions - In Pallette in Browser
-
-Make it Right Phase
--replan game
-	*flow
-	*to add
-	*frame per sec update (chars added must stay in their pos)
--draw characters
-	*draw line/circle char or append img?
-
-
-	
-drawing the board config might be done w/:
-	def _draw_cell(self, cell):
-		pygame.draw.rect(screen, self.line_color, cell)
-
-	def _make_cell(self):
-		for col in range(3):
-			for row in range(3):
-				self._draw_cell(pygame.Rect(row * self.cell_width, col * self.cell_height, self.cell_width, self.cell_height))
-		self._draw_cell()
-
-"""
